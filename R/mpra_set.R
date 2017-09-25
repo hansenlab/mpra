10f3ac433289a4052aa7c3727355ba95c702be75
Leslie Myint <leslie.myint@gmail.com>
2017-09-25 12:30:49 -0400

    Add na.rm = TRUE

--- R/mpra_set.R
+++ R/mpra_set.R
@@ -43,7 +43,7 @@ getDNA <- function(object, aggregate = FALSE) {
     raw <- assay(object, "DNA")
     if (aggregate) {
         eid <- getEid(object)
-        by_out <- by(raw, eid, colSums)
+        by_out <- by(raw, eid, colSums, na.rm = TRUE)
         agg <- do.call("rbind", by_out)
         rownames(agg) <- names(by_out)
         return(agg)
@@ -57,7 +57,7 @@ getRNA <- function(object, aggregate = FALSE) {
     raw <- assay(object, "RNA")
     if (aggregate) {
         eid <- getEid(object)
-        by_out <- by(raw, eid, colSums)
+        by_out <- by(raw, eid, colSums, na.rm = TRUE)
         agg <- do.call("rbind", by_out)
         rownames(agg) <- names(by_out)
         return(agg)
