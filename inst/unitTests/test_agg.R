d771229b1138a59594130b6b2b02bf8c6e76a2a2
Leslie Myint <leslie.myint@gmail.com>
2017-10-04 10:00:38 -0400

    Fix test names and ordering

--- inst/unitTests/test_agg.R
+++ inst/unitTests/test_agg.R
@@ -1,6 +1,11 @@
 test_agg <- function() {
     data(mpraSetExample)
     data(mpraSetAggExample)
-    checkEquals(getRNA(mpraSetExample, aggregate = TRUE), getDNA(mpraSetAggExample))
+    agg_manual <- getRNA(mpraSetExample, aggregate = TRUE)
+    agg_pre <- getRNA(mpraSetAggExample)
+    stopifnot(identical(dim(agg_manual), dim(agg_pre)))
+    agg_pre <- agg_pre[rownames(agg_manual),]
+    colnames(agg_pre) <- gsub("cond_sample_", "", colnames(agg_pre))
+    checkEquals(agg_manual, agg_pre)
 }
 
